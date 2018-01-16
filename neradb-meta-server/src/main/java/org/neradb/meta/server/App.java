91a3f50a9a597650c815b9e4b91d275e6984c907
jorgie <jorgie@qq.com>
2018-01-16 16:02:06 +0800

    init

---
+++ neradb-meta-server/src/main/java/org/neradb/meta/server/App.java
@@ -0,0 +1,13 @@
+package org.neradb.meta.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
