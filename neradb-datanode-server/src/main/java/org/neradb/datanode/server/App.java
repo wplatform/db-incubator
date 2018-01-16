1e8c89d99dc3341894ed86aa6bd6a8422bbf88ec
jorgie <jorgie@qq.com>
2018-01-16 15:29:22 +0800

    init

---
+++ neradb-datanode-server/src/main/java/org/neradb/datanode/server/App.java
@@ -0,0 +1,13 @@
+package org.neradb.datanode.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
